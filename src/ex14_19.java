39424241d029dfa8dbbb593e35c5dd8417d2cba8
Chris Bracey <cbracey5831@g.fmarion.edu>
2016-01-30 12:49:15 -0500

    Next Example

--- src/ex14_19.java
+++ src/ex14_19.java
@@ -4,6 +4,8 @@ import javafx.stage.Stage;
 import javafx.scene.shape.Polyline;
 import javafx.scene.layout.Pane;
 import javafx.collections.ObservableList;
+import javafx.scene.shape.Line;
+
 
 
 public class ex14_19 extends Application
@@ -13,7 +15,9 @@ public class ex14_19 extends Application
 	public void start(Stage primaryStage) throws Exception 
 	{
 		Polyline polyline = new Polyline();
+		Polyline polyline2 = new Polyline();
 		ObservableList<Double> list = polyline.getPoints();
+		ObservableList<Double> list2 = polyline2.getPoints();
 		double scaleFactor = 50;
 		for (int x = -170; x <= 170; x++) 
 		{
@@ -21,8 +25,28 @@ public class ex14_19 extends Application
 			list.add(100 - 50 * Math.sin((x / 100.0) * 2 * Math.PI));
 		}
 
+		for (int x = -170; x <= 170; x++) 
+		{
+			list2.add(x + 200.0);
+			list2.add(100 - 50 * Math.cos((x / 100.0) * 2 * Math.PI));
+		}
+
+		Line line = new Line();
+		line.setStartX(200.0f);
+		line.setStartY(25.0f);
+		line.setEndX(200.0f);
+		line.setEndY(170.0f);
+
+		Line line2 = new Line();
+		line2.setStartX(0.0f);
+		line2.setStartY(100.0f);
+		line2.setEndX(400.0f);
+		line2.setEndY(100.0f);
+
 		Pane pane = new Pane();
-		pane.getChildren().add(polyline);
+		pane.getChildren().addAll(polyline, polyline2, line, line2);
+
+
 		Scene scene = new Scene(pane);
 		primaryStage.setScene(scene);
 		primaryStage.show();
