905f2454f9d3e2796e2ac6f041b97f39078caa72
Chris <cbracey5831@g.fmarion.edu>
2016-01-31 13:51:22 -0500

    Finished ex14_19

--- src/ex14_19.java
+++ src/ex14_19.java
@@ -37,14 +37,66 @@ public class ex14_19 extends Application
 		line.setEndX(200.0f);
 		line.setEndY(170.0f);
 
+		Line topArrowRight = new Line();
+		topArrowRight.setStartX(200.0f);
+		topArrowRight.setStartY(25.0f);
+		topArrowRight.setEndX(205.0f);
+		topArrowRight.setEndY(35.0f);
+
+		Line topArrowLeft = new Line();
+		topArrowLeft.setStartX(200.0f);
+		topArrowLeft.setStartY(25.0f);
+		topArrowLeft.setEndX(195.0f);
+		topArrowLeft.setEndY(35.0);
+
+		Line bottomArrowLeft = new Line();
+		bottomArrowLeft.setStartX(200.0f);
+		bottomArrowLeft.setStartY(170.0f);
+		bottomArrowLeft.setEndX(205.0f);
+		bottomArrowLeft.setEndY(160.0f);
+
+		Line bottomArrowRight = new Line();
+		bottomArrowRight.setStartX(200.0f);
+		bottomArrowRight.setStartY(170.0f);
+		bottomArrowRight.setEndX(195.0f);
+		bottomArrowRight.setEndY(160.0f);
+
 		Line line2 = new Line();
 		line2.setStartX(0.0f);
 		line2.setStartY(100.0f);
 		line2.setEndX(400.0f);
 		line2.setEndY(100.0f);
 
+		Line leftArrowLeft = new Line();
+		leftArrowLeft.setStartX(0.0f);
+		leftArrowLeft.setStartY(100.0f);
+		leftArrowLeft.setEndX(10.0f);
+		leftArrowLeft.setEndY(105.0f);
+
+		Line leftArrowRight = new Line();
+		leftArrowRight.setStartX(0.0f);
+		leftArrowRight.setStartY(100.0f);
+		leftArrowRight.setEndX(10.0f);
+		leftArrowRight.setEndY(95.0f);
+
+
+		Line rightArrowLeft = new Line();
+		rightArrowLeft.setStartX(400.0f);
+		rightArrowLeft.setStartY(100.0f);
+		rightArrowLeft.setEndX(390.0f);
+		rightArrowLeft.setEndY(95.0f);
+
+		Line rightArrowRight = new Line();
+		rightArrowRight.setStartX(400.0f);
+		rightArrowRight.setStartY(100.0f);
+		rightArrowRight.setEndX(390.0f);
+		rightArrowRight.setEndY(105.0f);
+
 		Pane pane = new Pane();
-		pane.getChildren().addAll(polyline, polyline2, line, line2);
+		pane.getChildren().addAll(polyline, polyline2, line, line2, topArrowLeft, 
+								  topArrowRight, bottomArrowLeft, bottomArrowRight, 
+								  leftArrowLeft, leftArrowRight, rightArrowLeft, 
+								  rightArrowRight);
 
 
 		Scene scene = new Scene(pane);
